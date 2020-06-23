19a0ca768f1703623d248c5f045539f83a2640dd
Rupesh Chhetri <Rupesh_Chhetri@cable.comcast.com>
2020-06-23 10:06:49 -0400

    created RCNetworking project

---
+++ RCNetworking/RCNetworking/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RCNetworking
+//
+//  Created by Rupesh Chhetri on 6/23/20.
+//  Copyright © 2020 com.home.rupesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
+        
+    }
+
+
+}
+
